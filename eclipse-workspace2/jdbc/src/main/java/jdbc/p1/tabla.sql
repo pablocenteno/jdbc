0e0b669b4e1239b4c3efdf10875e41ec8488271e
Pablo <pablo.centeno2@educa.madrid.org>
2022-11-17 10:02:37 +0100

    segundo commit

--- eclipse-workspace2/jdbc/src/main/java/jdbc/p1/tabla.sql
+++ eclipse-workspace2/jdbc/src/main/java/jdbc/p1/tabla.sql
@@ -1,3 +1,4 @@
 create database practica1;
  use practica1;
 create table usuario(nombre varchar(25), apellidos varchar(25), dni varchar(9),fechanac date);
+create table vuelos2(id varchar(25), origen varchar(40), abrevSalida varchar(3), fechaSalida datetime(' YYYY-MM-DD hh:mm:ss'), destino varchar(25), abrevdestino varchar(3), fechaLlegada(datetime(" YYYY-MM-DD hh:mm:ss"), nPlazas int();
